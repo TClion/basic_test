522263a444a2d42c9ebe10e159352d8c8fa3a66d
TClion <763242665@qq.com>
2017-09-23 12:53:13 +0800

    Update pymongo_test.py

--- pymongo_test.py
+++ pymongo_test.py
@@ -12,7 +12,7 @@ import pymongo
 
 
 class Mongo():
-    def __init__(self,dbname,coll_name):
+    def __init__(self, dbname, coll_name):
         self.client = pymongo.MongoClient('localhost', 27017)    #连接数据库
         # self.db = self.client.dbname
         self.db = self.client[dbname]       #连接数据库
@@ -25,6 +25,17 @@ class Mongo():
         id = self.coll.insert(info)
         print id
 
+    #删除一条数据
+    def remove(self):
+        data = {'name': 'john'}
+        remove_info = self.coll.remove(data)
+        print remove_info
+
+    #更新数据
+    def update(self):
+        self.coll.update_many({'name': 'john'}, {'$set': {'age': '22'}})    #更新多条数据
+        self.coll.update({'name': 'john'}, {'$set': {'age': '22'}})         #更新一条数据
+
     #查找一条记录
     def find_one(self):
         info = self.coll.find_one()
@@ -36,6 +47,7 @@ class Mongo():
         loops = self.coll.find()
         for item in loops:
             print item
+            
     #排序
     def sort(self):
         loops = self.coll.find().sort('age', pymongo.DESCENDING)  #降序
@@ -45,12 +57,15 @@ class Mongo():
 
 if __name__ == '__main__':
     M = Mongo('school', 'student')      #dbname coll_name
-    #M.insert()
+    M.insert()
+    M.update()
+    # M.find()
+    M.find()
     # M.find_one()
-    #M.find()
-    print M.coll.count()      #查看数据个数
-    #M.coll.remove()     #删除表
-    M.sort()
+    # M.find()
+    # print M.coll.count()      #查看数据个数
+    # M.coll.remove()     #删除表
+    # M.sort()
 
 
 
